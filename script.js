e53c0764efa529d580fe12a67097efdd76cdfe7e
Anshulup84 <avenger720720@gmail.com>
2024-11-05 16:54:18 +0530

    Added hamburger and fix some response design issue in top navbar

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
 });
 
